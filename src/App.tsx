f624e838fb4badfe70c0afbb4ca6db0119a8b5a6
João Sanson <joaosanson01@gmail.com>
2023-09-07 20:00:16 -0300

    fix: fixing some imports on App.tsx :bug:

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components/'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header'
-import { InputTask } from './components/InputTask'
+import { InputTask } from './components/InputTask/index'
 
 export function App() {
   return (
