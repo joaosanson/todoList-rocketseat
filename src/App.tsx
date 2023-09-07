ccd66ab04c224e1593f5a7e5df2c532262073c5c
João Sanson <joaosanson01@gmail.com>
2023-09-07 11:09:36 -0300

    feat: cleaning project to recreate app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-// import styles from './App.module.css';
-import './global.css'
-
-import { Header } from './components/Header'
-import { Form } from './components/Form'
-import { List } from './components/List'
-import { TodosProvider } from './context/ContextTodos'
-
 function App() {
-  return (
-    <TodosProvider>
-      <Header />
-      <Form />
-      <List />
-    </TodosProvider>
-  )
+  return <h1></h1>
 }
 
 export default App
